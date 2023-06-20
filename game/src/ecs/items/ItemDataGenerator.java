73c1d607950c257503a2c3752e2a34745887feef
Pascal378 <pascal.schwartz@protonmail.com>
2023-06-20 19:19:49 +0200

    Spotless apply

--- game/src/ecs/items/ItemDataGenerator.java
+++ game/src/ecs/items/ItemDataGenerator.java
@@ -3,34 +3,31 @@ package ecs.items;
 import ecs.graphic.Animation;
 import ecs.items.newItems.Greatsword;
 import ecs.items.newItems.InvinciblePotion;
-
 import java.util.List;
 import java.util.Random;
 
-/**
- * Generator which creates a random ItemData based on the Templates prepared.
- */
+/** Generator which creates a random ItemData based on the Templates prepared. */
 public class ItemDataGenerator {
 
     private static final List<String> bookOfRa = List.of("item/drop/BookOfRa/BookOfRa.png");
     private static final List<String> greatsword = List.of("item/drop/Greatsword/greatsword.png");
     private static final List<String> invinciblePotion =
-        List.of("item/drop/InvinciblePotion/InvinciblePotion_anim_f0.png");
+            List.of("item/drop/InvinciblePotion/InvinciblePotion_anim_f0.png");
 
     private List<ItemData> templates =
-        List.of(
-            new Greatsword(
-                ItemType.Passive,
-                new Animation(greatsword, 1),
-                new Animation(greatsword, 1),
-                "Greatsword",
-                "Increases the owners damage by 20"),
-            new InvinciblePotion(
-                ItemType.Active,
-                new Animation(invinciblePotion, 1),
-                new Animation(invinciblePotion, 1),
-                "Invincible Potion",
-                "A Potion which makes you immortal for 5 seconds"));
+            List.of(
+                    new Greatsword(
+                            ItemType.Passive,
+                            new Animation(greatsword, 1),
+                            new Animation(greatsword, 1),
+                            "Greatsword",
+                            "Increases the owners damage by 20"),
+                    new InvinciblePotion(
+                            ItemType.Active,
+                            new Animation(invinciblePotion, 1),
+                            new Animation(invinciblePotion, 1),
+                            "Invincible Potion",
+                            "A Potion which makes you immortal for 5 seconds"));
     private Random rand = new Random();
 
     /**
